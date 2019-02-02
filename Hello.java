76f3e54a4b8b3bab8d4155d246dbd2b515a75d14
anjeshgoud <35951676+anjeshgoud@users.noreply.github.com>
2019-02-02 09:37:40 -0500

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,36 @@
+package core.java;
+
+public class Hello {
+	
+	
+			
+	public static void main (String[] args) {
+		
+		 //Student[] s= new Student[10];
+		int[] a;
+		a= new int[10];
+		//String[] s=new String[10];
+		a[0]=9;
+		a[1]=4;
+		System.out.println(a);
+		
+		for(int i=0;i<10;i++)		{
+			a[i]=i;
+			
+		}
+		
+		for(int i=0;i<10;i++)		{
+			System.out.print(a[i]);
+			
+		}
+		
+		System.out.println();
+		
+		//int String=06;
+		//System.out.println(String);
+		System.out.println ("Hello Anjesh");
+		
+		
+
+}
+}
